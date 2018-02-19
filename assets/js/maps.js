d907cc3b529aec9bcc05347adde163d0597e98f8
evanm31 <ermoore@umass.edu>
2018-02-19 17:32:11 -0500

    updates to homepage, maps, and file structure

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -11,15 +11,15 @@
             var marker2 = L.marker([42.4, -72.51]).addTo(mymap);
             var marker3 = L.marker([42.35, -72.525]).addTo(mymap);
 
-            marker1.bindPopup("<b>Username</b><br>Currently Listening").openPopup();
-            marker2.bindPopup("<b>Username</b><br>Currently Listening").openPopup();
-            marker3.bindPopup("<b>Username</b><br>Currently Listening").openPopup();
+            marker1.bindPopup("<b>guitardude53</b><br>Listening to: Fire by Jimi Hendrix<br>Link to profile page").openPopup();
+            marker2.bindPopup("<b>TimRichards</b><br>Listening to: Africa by Toto<br>Link to profile page").openPopup();
+            marker3.bindPopup("<b>JonesC</b><br>Listening to: Shape of You by Ed Sheeran<br>Link to profile page").openPopup();
 
             var marker4 = L.marker([42.41, -72.6]).addTo(mymap);
             var marker5 = L.marker([42.39, -72.58]).addTo(mymap);
             var marker6 = L.marker([42.43, -72.55]).addTo(mymap);
 
-            marker1.bindPopup("<b>Event Title</b><br>Date<br>Link to more info").openPopup();
-            marker2.bindPopup("<b>Event Title</b><br>Date<br>Link to more info").openPopup();
-            marker3.bindPopup("<b>Event Title</b><br>Date<br>Link to more info").openPopup();
+            marker4.bindPopup("<b>House Party</b><br>2/19/18<br>Link to event page").openPopup();
+            marker5.bindPopup("<b>Jazz Concert</b><br>2/25/18<br>Link to event page").openPopup();
+            marker6.bindPopup("<b>Open Mic Night</b><br>2/15/18<br>Link to event page").openPopup();
 

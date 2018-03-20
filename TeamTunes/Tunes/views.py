7990d1b9cac805b34f8d88d79cec8fb9eadfebf5
kgodinho <kgodinho56@gmail.com>
2018-03-20 17:22:02 -0400

    Automate Users

--- TeamTunes/Tunes/views.py
+++ TeamTunes/Tunes/views.py
@@ -2,12 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Event
+from random import randint
+
+from .models import User, Event, Song, Album, Genre, Artist, Location
 
 def index(request):
     """
     For the Home Page
     """
     #Get List of random users
+    num_users = User.objects.all().count()
+    num = 0
+    users = []
+    hold = User.objects.order_by('?')
+
+    for user in hold:
+        if num > 5 or num > num_users-1:
+            break
+        users.append(user)
+        num = num + 1
+
+
+    events = []
 
     #Get List of random Events
 
@@ -16,7 +32,7 @@ def index(request):
     return render(
     request,
     'index.html',
-    context={}
+    context={"user": users, "events": events, "num": num_users}
     )
 
 from django.views import generic
